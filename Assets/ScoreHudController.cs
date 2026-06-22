29079bd60d8084436f484f55de27469011f3038b
hailer0112 <hailer0112@gmail.com>
2026-06-22 17:44:37 +0900

    button

---
+++ Assets/ScoreHudController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using TMPro;
+
+public class ScoreHudController : MonoBehaviour
+{
+    [SerializeField] private TMP_Text scoreText;
+
+    private int score;
+    void Start()
+    {
+        RefreshScoreText();
+    }
+
+    public void AddScore()
+    {
+        score++;
+        RefreshScoreText();
+    }
+
+    private void RefreshScoreText()
+    {
+        scoreText.text = $"Score: {score}";
+    }
+
+
+
+
+}
